d405de2e28419bcee28629af6e4d926bad0ee760
Mr2Much <anubis.lockward@gmail.com>
2018-01-31 00:50:53 -0400

    Added some Menus for album and playlist management.

--- src/com/colddog/Main.java
+++ src/com/colddog/Main.java
@@ -6,21 +6,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static Scanner scanner = new Scanner(System.in);
-    public static boolean quit = false;
-    public ArrayList<Album> albums;
+    private static Scanner scanner = new Scanner(System.in);
+    private static boolean quit = false;
+    private ArrayList<Album> albums;
 
     public static void main(String[] args) {
         int choice;
-        printMenuOptions();
+
 	    while(!quit) {
+	        printMenuOptions();
+            System.out.println();
             System.out.print("Enter your choice: ");
 	        choice = scanner.nextInt();
 
 	        switch (choice) {
                 case 1:
+                    manageAlbums();
                     break;
                 case 2:
+                    managePlaylists();
                     break;
 	            case 0:
                     quit = true;
@@ -35,10 +39,38 @@ public class Main {
     }
 
     public static void printMenuOptions() {
-        System.out.println("Choose one of the following options:\n" +
+        System.out.printf("%1$32s\n", "Main Menu");
+        System.out.print("Choose one of the following options:\n" +
                 "1 - Manage Albums.\n" +
                 "2 - Manage Playlists.\n" +
                 "3 - Show menu options.\n" +
-                "0 - Exit Program.");
+                "0 - Exit Program.\n");
+    }
+
+    public static void manageAlbums() {
+        int choice = 1;
+
+        printAlbumOptions();
+
+        while(choice > 0) {
+            System.out.println("Enter choice: ");
+            choice = scanner.nextInt();
+        }
+    }
+
+    public static void printAlbumOptions() {
+        System.out.printf("%1$32s\n", "Album Management Menu");
+        System.out.println("What do you wish to do:\n" +
+                "1 - Create new Album.\n" +
+                "2 - Edit existing Album name.\n" +
+                "3 - Show all Albums.\n" +
+                "4 - Show All Albums and All Songs.\n" +
+                "5 - Select existing Album to manage Songs.\n" +
+                "6 - Show Album management options.\n" +
+                "0 - Exit Album Management Menu.\n");
+    }
+
+    public static void managePlaylists() {
+        System.out.printf("%1$32s\n", "Playlist Management Menu");
     }
 }
